69691a728c697d1a2e4eb364c374d2a41027ec85
Darya Parygina <pa_darochek@ispras.ru>
2022-06-01 18:11:51 +0300

    OpenCV

---
+++ projects/opencv/main.c
@@ -0,0 +1,34 @@
+// Copyright 2021 ISP RAS
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size);
+
+int main(int argc, char** argv)
+{
+  FILE* fd = fopen(argv[1], "rb");
+  if (!fd) return 1;
+  fseek(fd, 0, SEEK_END);
+  long fsize = ftell(fd);
+  fseek(fd, 0, SEEK_SET);
+  char* buffer = (char*)malloc(fsize);
+  fread(buffer, 1, fsize, fd);
+  fclose(fd);
+  return LLVMFuzzerTestOneInput((const uint8_t*)buffer, fsize);
+}
