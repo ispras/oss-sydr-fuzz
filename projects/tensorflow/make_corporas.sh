9fb95eb943d2cda4f8fa561bbeff22902ffb1a78
Eli Kobrin <kobrineli@ispras.ru>
2022-08-21 20:11:25 +0300

    Remove archieves.

--- projects/tensorflow/make_corporas.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget https://pumba.intra.ispras.ru/sydr-www/tensorflow/$1_fuzz/$1_fuzz-out.tar.xz
-tar -xf $1_fuzz-out.tar.xz
-mv $1_fuzz-out/corpus $1
-zip $1.zip $1/*
-rm -rf $1_fuzz-out.tar.xz $1_fuzz-out $1
