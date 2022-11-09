b470a9711eef0d5cc514f09ef415a24fec95e788
Avgor46 <avgor46@ispras.ru>
2022-11-09 20:08:26 +0300

    Fixes

--- projects/nodejs/build.sh
+++ projects/nodejs/build.sh
@@ -20,15 +20,15 @@ cd /node_libfuzzer
 
 export CC=clang
 export CXX=clang++
-export CXXFLAGS="-g -fsanitize=fuzzer-no-link,address,integer,undefined,bounds,null,float-divide-by-zero"
-export CFLAGS=$CXXFLAGS
+export CXXFLAGS="-g -std=c++17 -fsanitize=fuzzer-no-link,address,integer,undefined,bounds,null,float-divide-by-zero"
+export CFLAGS="-g -fsanitize=fuzzer-no-link,address,integer,undefined,bounds,null,float-divide-by-zero"
 export LDFLAGS="-latomic $CXXFLAGS"
 
 ./configure
 make -j$(nproc)
 ar -rcT static.a $(find . -name "*.o")
 
-export CXXFLAGS="-g -fsanitize=fuzzer,address,integer,undefined,bounds,null,float-divide-by-zero"
+export CXXFLAGS="-g -std=c++17 -fsanitize=fuzzer,address,integer,undefined,bounds,null,float-divide-by-zero"
 NODE_WANT_INTERNALS=1 $CXX $CXXFLAGS -pthread test/fuzzers/fuzz_env.cc -o /load_env_fuzzer -DNODE_WANT_INTERNALS \
      -I./deps/v8/include -I./src/ -I./test/fuzzers/ -I./deps/uv/include/ static.a -ldl -latomic -fno-rtti
 NODE_WANT_INTERNALS=1 $CXX $CXXFLAGS -pthread test/fuzzers/fuzz_url.cc -o /load_url_fuzzer -DNODE_WANT_INTERNALS \
@@ -49,7 +49,7 @@ ar -rcT static.a $(find . -name "*.o")
 
 $CXX $CXXFLAGS -pthread v8_compile.cpp -o /v8_compile_afl -I./deps/v8/include -I./deps/v8/include/libplatform ./static.a -ldl
 
-export CXXFLAGS="-g -fsanitize=fuzzer,address,integer,undefined,bounds,null,float-divide-by-zero"
+export CXXFLAGS="-g -std=c++17 -fsanitize=fuzzer,address,integer,undefined,bounds,null,float-divide-by-zero"
 $CXX $CXXFLAGS -pthread test/fuzzers/fuzz_env.cc -o /load_env_afl -DNODE_WANT_INTERNALS \
     -I./deps/v8/include -I./src/ -I./test/fuzzers/ -I./deps/uv/include/ static.a -ldl -latomic -fno-rtti
 $CXX $CXXFLAGS -pthread test/fuzzers/fuzz_url.cc -o /load_url_afl -DNODE_WANT_INTERNALS \
@@ -70,9 +70,9 @@ make -j$(nproc)
 ar -rcT static.a $(find . -name "*.o")
 $CXX $CXXFLAGS -pthread v8_compile_sydr.cpp -o /v8_compile_sydr \
     -I./deps/v8/include -I./deps/v8/include/libplatform  ./static.a -ldl
-$CXX $CXXFLAGS -pthread /sydr_main.cc test/fuzzers/fuzz_env.cc -o /load_env_sydr -DNODE_WANT_INTERNALS \
-    -DUSE_INITIALIZE -I./deps/v8/include -I./src/ -I./test/fuzzers/ -I./deps/uv/include/ static.a -ldl -latomic -fno-rtti
-$CXX $CXXFLAGS -pthread /sydr_main.cc test/fuzzers/fuzz_url.cc -o /load_url_sydr -DNODE_WANT_INTERNALS \
+$CXX $CXXFLAGS -pthread /opt/StandaloneFuzzTargetMain.c test/fuzzers/fuzz_env.cc -o /load_env_sydr -DNODE_WANT_INTERNALS \
+    -I./deps/v8/include -I./src/ -I./test/fuzzers/ -I./deps/uv/include/ static.a -ldl -latomic -fno-rtti
+$CXX $CXXFLAGS -pthread /opt/StandaloneFuzzTargetMain.c test/fuzzers/fuzz_url.cc -o /load_url_sydr -DNODE_WANT_INTERNALS \
     -I./deps/v8/include -I./src/ -I./test/fuzzers/ -I./deps/uv/include/ static.a -ldl -latomic -fno-rtti
 
 # coverage
@@ -89,8 +89,8 @@ export LDFLAGS="-latomic $CXXFLAGS"
 make -j$(nproc)
 ar -rcT static.a $(find . -name "*.o")
 $CXX $CXXFLAGS -pthread v8_compile_sydr.cpp -o /v8_compile_cov \
-   -I./deps/v8/include -I./deps/v8/include/libplatform ./static.a -ldl
-$CXX $CXXFLAGS -pthread /sydr_main.cc test/fuzzers/fuzz_env.cc -o /load_env_cov -DNODE_WANT_INTERNALS \
-    -DUSE_INITIALIZE -I./deps/v8/include -I./src/ -I./test/fuzzers/ -I./deps/uv/include/ static.a -ldl -latomic -fno-rtti
-$CXX $CXXFLAGS -pthread /sydr_main.cc test/fuzzers/fuzz_url.cc -o /load_url_cov -DNODE_WANT_INTERNALS \
+    -I./deps/v8/include -I./deps/v8/include/libplatform ./static.a -ldl
+$CXX $CXXFLAGS -pthread /opt/StandaloneFuzzTargetMain.c test/fuzzers/fuzz_env.cc -o /load_env_cov -DNODE_WANT_INTERNALS \
+    -I./deps/v8/include -I./src/ -I./test/fuzzers/ -I./deps/uv/include/ static.a -ldl -latomic -fno-rtti
+$CXX $CXXFLAGS -pthread /opt/StandaloneFuzzTargetMain.c test/fuzzers/fuzz_url.cc -o /load_url_cov -DNODE_WANT_INTERNALS \
     -I./deps/v8/include -I./src/ -I./test/fuzzers/ -I./deps/uv/include/ static.a -ldl -latomic -fno-rtti
