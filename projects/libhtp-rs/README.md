691fe754b5db24417838dc1c47306f4b9b6ffab5
koltiradw <koltiradw@yandex.ru>
2022-12-14 02:46:04 +0300

    add libhtp-rs project

---
+++ projects/libhtp-rs/README.md
@@ -0,0 +1,41 @@
+# libhtp-rs
+
+Experimental c2rust conversion of OISF/libhtp
+
+## Build Docker
+
+    $ sudo docker build -t oss-sydr-fuzz-libhtp-rs .
+
+## Run Hybrid Fuzzing
+
+Unzip Sydr (`sydr.zip`) in `projects/libhtp-rs` directory:
+
+    $ unzip sydr.zip
+
+Run docker:
+
+    $ sudo docker run --privileged --network host -v /etc/localtime:/etc/localtime:ro --rm -it -v $PWD:/fuzz oss-sydr-fuzz-libhtp-rs /bin/bash
+
+Change directory to `/fuzz`:
+
+    # cd /fuzz
+
+Run hybrid fuzzing with libfuzzer:
+
+    # sydr-fuzz -c sydr-fuzz.toml run
+
+Run hybrid fuzzing with afl++:
+
+    # sydr-fuzz -c sydr-fuzz-afl++.toml run
+
+Minimize corpus:
+
+    # sydr-fuzz -c sydr-fuzz.toml cmin
+
+Check security predicates:
+
+    # sydr-fuzz -c sydr-fuzz.toml security
+
+## Supported Targets
+
+    * fuzz_htp
---
+++ projects/libhtp-rs/build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -eu
+
+cd /libhtp-rs/fuzz/
+
+export CARGO_BUILD_TARGET="x86_64-unknown-linux-gnu"
+export RUSTFLAGS="-C debug-assertions -C overflow_checks -C debuginfo=2"
+
+cargo +nightly fuzz build -O
+cp target/x86_64-unknown-linux-gnu/release/fuzz_htp_rs /cargo_fuzz_htp_rs
+
+unset RUSTFLAGS
+
+cd /libhtp-rs/afl/
+
+export CARGO_BUILD_TARGET="x86_64-unknown-linux-gnu"
+
+cargo afl build
+cp target/x86_64-unknown-linux-gnu/debug/fuzz_htp_rs /afl_fuzz_htp_rs
+
+cd /libhtp-rs/sydr/
+
+export CARGO_BUILD_TARGET="x86_64-unknown-linux-gnu"
+cargo build
+cp target/x86_64-unknown-linux-gnu/debug/sydr_htp_rs /sydr_fuzz_htp_rs
+
+unset CARGO_BUILD_TARGET
+
+cd /libhtp-rs/sydr/
+
+export RUSTFLAGS="-C instrument-coverage"
+
+cargo build
+cp target/x86_64-unknown-linux-gnu/debug/sydr_htp_rs /cover_fuzz_htp_rs
