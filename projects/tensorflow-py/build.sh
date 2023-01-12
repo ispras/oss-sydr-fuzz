f268f364373bb05e4063b41b7e5e10acb832348f
Darya Parygina <pa_darochek@ispras.ru>
2023-01-12 16:31:40 +0300

    Fix commit + delete fuzzers build

--- projects/tensorflow-py/build.sh
+++ projects/tensorflow-py/build.sh
@@ -28,24 +28,3 @@ bazel clean --expunge
 bazel build --spawn_strategy=sandboxed //tensorflow/tools/pip_package:build_pip_package
 ./bazel-bin/tensorflow/tools/pip_package/build_pip_package --nightly_flag /tmp/tensorflow_pkg
 python3 -m pip install /tmp/tensorflow_pkg/tf_nightly-2.12.0-cp38-cp38-linux_x86_64.whl
-
-# Rename to avoid the following: https://github.com/tensorflow/tensorflow/issues/40182
-mv /tensorflow/tensorflow /tensorflow/tensorflow_src
-
-# Build fuzzers into $OUT. These could be detected in other ways.
-
-for fuzzer in $(find / -name '*_fuzz.py'); do
-  fuzzer_basename=$(basename -s .py $fuzzer)
-  fuzzer_package=${fuzzer_basename}.pkg
-
-  pyinstaller --distpath $OUT --onefile --name $fuzzer_package $fuzzer
-
-  echo "#!/bin/sh
-# LLVMFuzzerTestOneInput for fuzzer detection.
-this_dir=\$(dirname \"\$0\")
-LD_PRELOAD=ASAN_OPTIONS=\$ASAN_OPTIONS:symbolize=1:external_symbolizer_path=\$this_dir/llvm-symbolizer:detect_leaks=0 \
-\$this_dir/$fuzzer_package \$@" > $OUT/$fuzzer_basename
-  chmod +x $OUT/$fuzzer_basename
-done
-
-mv /tensorflow/tensorflow_src /tensorflow/tensorflow
