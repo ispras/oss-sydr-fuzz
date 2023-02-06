892044d6203b892550cd6968c93aaf2d1ba73853
headshog <124502670+headshog@users.noreply.github.com>
2023-02-06 18:55:59 +0300

    Update build.sh

--- projects/lighttpd/build.sh
+++ projects/lighttpd/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -eu
 # Copyright 2021 Google LLC
+# Modifications copyright (C) 2023 ISP RAS
 #
 # Modifications copyright (C) 2023 ISP RAS
 # Licensed under the Apache License, Version 2.0 (the "License");
