94bc5f262b1c590fa10f4082ef254897946e992f
headshog <124502670+headshog@users.noreply.github.com>
2023-02-06 16:40:38 +0300

    Update build.sh

--- projects/lighttpd/build.sh
+++ projects/lighttpd/build.sh
@@ -39,8 +39,8 @@ make clean
 make
 cd src
 
-$CC $CFLAGS -c /fuzz_burl_cov.c -I. -I../include
-$CXX $CXXFLAGS fuzz_burl_cov.o lighttpd-burl.o lighttpd-buffer.o lighttpd-base64.o lighttpd-ck.o -o /fuzz_burl_cov
+$CC $CFLAGS -c /fuzz_burl_sydr.c -I. -I../include
+$CXX $CXXFLAGS fuzz_burl_sydr.o lighttpd-burl.o lighttpd-buffer.o lighttpd-base64.o lighttpd-ck.o -o /fuzz_burl_cov
 
 export CXXFLAGS="-g"
 export CFLAGS="-g"
@@ -51,5 +51,5 @@ make clean
 make
 cd src
 
-$CC $CFLAGS -c /fuzz_burl_cov.c -I. -I../include
-$CXX $CXXFLAGS fuzz_burl_cov.o lighttpd-burl.o lighttpd-buffer.o lighttpd-base64.o lighttpd-ck.o -o /fuzz_burl_sydr
+$CC $CFLAGS -c /fuzz_burl_sydr.c -I. -I../include
+$CXX $CXXFLAGS fuzz_burl_sydr.o lighttpd-burl.o lighttpd-buffer.o lighttpd-base64.o lighttpd-ck.o -o /fuzz_burl_sydr
