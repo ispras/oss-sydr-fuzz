4ba6cd0da4060699b5640bcf6697cd850df961b6
Avgor46 <avgor46@ispras.ru>
2023-05-31 14:56:54 +0300

    Added open-jdk to aarch64 docker

--- projects/tensorflow/install_java.sh
+++ projects/tensorflow/install_java.sh
@@ -24,15 +24,4 @@ tar -xzv --strip-components=1 -f openjdk-15.0.2_linux-x64_bin.tar.gz --directory
 rm -f openjdk-15.0.2_linux-x64_bin.tar.gz
 rm -rf $JAVA_HOME/jmods $JAVA_HOME/lib/src.zip
 
-# Install the latest Jazzer in $OUT.
-# jazzer_api_deploy.jar is required only at build-time, the agent and the
-# drivers are copied to $OUT as they need to be present on the runners.
-cd /
-git clone --depth=1 https://github.com/CodeIntelligenceTesting/jazzer && \
-cd jazzer
-bazel build --java_runtime_version=localjdk_15 -c opt --cxxopt="-stdlib=libc++" --linkopt=-lc++ \
-  //agent:jazzer_agent_deploy.jar //driver:jazzer_driver //driver:jazzer_driver_asan //driver:jazzer_driver_ubsan //agent:jazzer_api_deploy.jar
-cp bazel-bin/agent/jazzer_agent_deploy.jar bazel-bin/driver/jazzer_driver bazel-bin/driver/jazzer_driver_asan bazel-bin/driver/jazzer_driver_ubsan /usr/local/bin/
-cp bazel-bin/agent/jazzer_api_deploy.jar $JAZZER_API_PATH
 rm -rf ~/.cache/bazel ~/.cache/bazelisk
-rm -rf /jazzer
