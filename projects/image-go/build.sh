dc46a400ea3691b39e8b459ebfe450d0bc632d04
Daniil Kutz <kutz@ispras.ru>
2023-06-21 19:02:29 +0300

    [image-go] Add targets for coverage

--- projects/image-go/build.sh
+++ projects/image-go/build.sh
@@ -3,24 +3,29 @@
 # webp
 go-fuzz-build -libfuzzer -func=FuzzWebp -o webp.a
 clang -fsanitize=fuzzer webp.a -o fuzz_webp
+go-fuzz-build -func=FuzzWebp -o fuzz_webp.zip
 cd cmd/sydr_webp && go build && cd -
 
 # tiff
 go-fuzz-build -libfuzzer -func=FuzzTiff -o tiff.a
 clang -fsanitize=fuzzer tiff.a -o fuzz_tiff
+go-fuzz-build -func=FuzzTiff -o fuzz_tiff.zip
 cd cmd/sydr_tiff && go build && cd -
 
 # png
 go-fuzz-build -libfuzzer -func=FuzzPng -o png.a
 clang -fsanitize=fuzzer png.a -o fuzz_png
+go-fuzz-build -func=FuzzPng -o fuzz_png.zip
 cd cmd/sydr_png && go build && cd -
 
 # jpeg
 go-fuzz-build -libfuzzer -func=FuzzJpeg -o jpeg.a
 clang -fsanitize=fuzzer jpeg.a -o fuzz_jpeg
+go-fuzz-build -func=FuzzJpeg -o fuzz_jpeg.zip
 cd cmd/sydr_jpeg && go build && cd -
 
 # gif
 go-fuzz-build -libfuzzer -func=FuzzGif -o gif.a
 clang -fsanitize=fuzzer gif.a -o fuzz_gif
+go-fuzz-build -func=FuzzGif -o fuzz_gif.zip
 cd cmd/sydr_gif && go build && cd -
