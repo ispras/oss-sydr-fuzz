7bb9ae42b508b9da61c59469f168fedf6cb6056a
headshog <craaaaaachind@gmail.com>
2023-07-24 16:09:10 +0300

    tcpdump project fixes

--- projects/tcpdump/build.sh
+++ projects/tcpdump/build.sh
@@ -16,59 +16,54 @@
 #
 ################################################################################
 
-#Build targets for libfuzzer
 if [[ $CONFIG = "libfuzzer" ]]
 then
     export CC="clang"
     export CFLAGS="-g -fsanitize=fuzzer-no-link,address,bounds,integer,undefined,null,float-divide-by-zero"
-    export TARGET_CFLAGS="-g -fsanitize=fuzzer,address,bounds,integer,undefined,null,float-divide-by-zero"
-    export LINK_FLAGS="$TARGET_CFLAGS -I/libpcap -I/tcpdump ../load_sydr_packet.c -o ../fuzzer_packet\
+    export LINK_FLAGS="-g -fsanitize=fuzzer,address,bounds,integer,undefined,null,float-divide-by-zero\
+                       -I/libpcap -I/tcpdump ../pretty_print_packet.c -o ../pretty_print_packet_fuzz\
                        /libpcap/libpcap.a /tcpdump/libnetdissect.a"
 fi
 
 if [[ $CONFIG = "afl" ]]
 then
-    cd /libpcap && make clean
-    cd /tcpdump && make clean
     export CC="afl-clang-fast"
     export CFLAGS="-g -fsanitize=address,bounds,integer,undefined,null,float-divide-by-zero"
-    export TARGET_CFLAGS="-g -fsanitize=fuzzer,address,bounds,integer,undefined,null,float-divide-by-zero"
-    export LINK_FLAGS="$TARGET_CFLAGS -I/libpcap -I/tcpdump ../load_sydr_packet.c -o ../afl_packet\
+    export LINK_FLAGS="-g -fsanitize=fuzzer,address,bounds,integer,undefined,null,float-divide-by-zero\
+                       -I/libpcap -I/tcpdump ../pretty_print_packet.c -o ../pretty_print_packet_afl\
                        /libpcap/libpcap.a /tcpdump/libnetdissect.a"
 fi
 
 if [[ $CONFIG = "sydr" ]]
 then
-    cd /libpcap && make clean
-    cd /tcpdump && make clean
     export CC="clang"
     export CFLAGS="-g"
-    export LINK_FLAGS="$CFLAGS -I/libpcap -I/tcpdump ../load_sydr_packet.c -o ../sydr_packet\
+    export LINK_FLAGS="$CFLAGS -I/libpcap -I/tcpdump -DSYDR ../pretty_print_packet.c -o ../pretty_print_packet_sydr\
                        /libpcap/libpcap.a /tcpdump/libnetdissect.a /main.o"
-    $CC $CFLAGS -c -o /main.o /opt/StandaloneFuzzTargetMain.c
+    $CC $CFLAGS -c -o /main.o /main.c
 fi
 
 if [[ $CONFIG = "coverage" ]]
 then
-    cd /libpcap && make clean
-    cd /tcpdump && make clean
     export CC="clang"
     export CFLAGS="-fprofile-instr-generate -fcoverage-mapping"
-    export LINK_FLAGS="$CFLAGS -I/libpcap -I/tcpdump ../load_sydr_packet.c -o ../cov_packet\
+    export LINK_FLAGS="$CFLAGS -I/libpcap -I/tcpdump -DSYDR ../pretty_print_packet.c -o ../pretty_print_packet_cov\
                        /libpcap/libpcap.a /tcpdump/libnetdissect.a /main.o"
-    $CC $CFLAGS -c -o main.o /opt/StandaloneFuzzTargetMain.c
+    $CC $CFLAGS -c -o /main.o /main.c
 fi
 
 cd /libpcap
 echo "build libpcap"
 ./autogen.sh
 ./configure CC="$CC" CFLAGS="$CFLAGS"
+make clean
 make -j`nproc`
 
 cd /tcpdump
 echo "build tcpdump"
 ./autogen.sh
 ./configure CC="$CC" CFLAGS="$CFLAGS"
+make clean
 make -j`nproc`
 
 $CC $LINK_FLAGS
--- projects/tcpdump/pretty_print_packet.c
+++ projects/tcpdump/pretty_print_packet.c
@@ -1,3 +1,19 @@
+// Copyright 2023 ISP RAS
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
 #include <pcap/pcap.h>
 #include <netdissect-stdinc.h>
 #include <netdissect.h>
@@ -67,7 +83,11 @@ int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
   ndo->ndo_Aflag++;
   ndo->ndo_Hflag++;
 
+#ifdef SYDR
+  fd = fopen(Data, "r");
+#else
 	fd = bufferToFile(Data, Size);
+#endif
  	if (fd == NULL)
     		return 0;
 
@@ -88,4 +108,3 @@ int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
  	nd_cleanup();	
 	return 0;
 }
-
