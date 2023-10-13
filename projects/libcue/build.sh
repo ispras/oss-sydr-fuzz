b1d1262117d2be08b3b5013a3455bf1625481476
Kobrin Ilay <kobrineli@ispras.ru>
2023-10-13 13:36:18 +0300

    Update build.sh

--- projects/libcue/build.sh
+++ projects/libcue/build.sh
@@ -1,3 +1,4 @@
+#!/bin/bash -eu
 # Copyright 2023 ISP RAS
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,8 +14,6 @@
 # limitations under the License.
 #
 ################################################################################
-
-#!/bin/bash -eu
 
 cd /libcue
 
