3c88bd3710cc66c3fa5753d35f5157d8d3bb65e0
fedotoff <fedotoff@ispras.ru>
2023-10-23 17:33:00 +0300

    update example

--- projects/libwebp/build.sh
+++ projects/libwebp/build.sh
@@ -16,15 +16,49 @@
 #
 ################################################################################
 
-#libFuzzer
-export CC=clang
-export CXX=clang++
-CFLAGS="-fsanitize=fuzzer-no-link,address,bounds,integer,undefined,null,float-divide-by-zero -g"
-CXXFLAGS="-fsanitize=fuzzer-no-link,address,bounds,integer,undefined,null,float-divide-by-zero -g"
-
+export AFL_LLVM_ALLOWLIST=/libwebp.list
 # limit allocation size to reduce spurious OOMs
-WEBP_CFLAGS="$CFLAGS -DWEBP_MAX_IMAGE_SIZE=838860800" # 800MiB
+export WEBP_CFLAGS="-DWEBP_MAX_IMAGE_SIZE=838860800" # 800MiB
 
+for i in cmplog compcov ctx ngram asan; do
+    cp -r libwebp libwebp-$i
+done
+
+
+# Sydr
+cd libwebp
+./autogen.sh
+CC=clang CFLAGS="-g $WEBP_CFLAGS" ./configure \
+  --enable-asserts \
+  --enable-libwebpdemux \
+  --enable-libwebpmux \
+  --disable-shared \
+  --disable-jpeg \
+  --disable-tiff \
+  --disable-gif \
+  --disable-wic
+make clean
+make -j$(nproc)
+
+# Cmplog
+cd ../libwebp-cmplog
+git apply vhtc.patch
+./autogen.sh
+CC=clang CFLAGS="$WEBP_CFLAGS" ./configure \
+  --enable-asserts \
+  --enable-libwebpdemux \
+  --enable-libwebpmux \
+  --disable-shared \
+  --disable-jpeg \
+  --disable-tiff \
+  --disable-gif \
+  --disable-wic
+make clean
+make CC=afl-clang-fast AFL_LLVM_CMPLOG=1 -j$(nproc)
+
+# Asan
+cd ../libwebp-asan
+git apply vhtc.patch
 ./autogen.sh
 CFLAGS="$WEBP_CFLAGS" ./configure \
   --enable-asserts \
@@ -36,52 +70,12 @@ CFLAGS="$WEBP_CFLAGS" ./configure \
   --disable-gif \
   --disable-wic
 make clean
-make -j$(nproc)
+make CC=afl-clang-fast AFL_USE_ASAN=1 -j$(nproc)
 
-webp_libs=(
-  src/demux/.libs/libwebpdemux.a
-  src/mux/.libs/libwebpmux.a
-  src/.libs/libwebp.a
-  imageio/.libs/libimageio_util.a
-  sharpyuv/.libs/libsharpyuv.a
-)
-webp_c_fuzzers=(
-  advanced_api_fuzzer
-  animation_api_fuzzer
-  huffman_fuzzer
-  mux_demux_api_fuzzer
-  simple_api_fuzzer
-)
-webp_cxx_fuzzers=(
-  animdecoder_fuzzer
-  animencoder_fuzzer
-  enc_dec_fuzzer
-)
-LIB_FUZZING_ENGINE="-fsanitize=fuzzer"
-for fuzzer in "${webp_c_fuzzers[@]}"; do
-  $CC $CFLAGS -Isrc -I. tests/fuzzer/${fuzzer}.c -c -o tests/fuzzer/${fuzzer}.o
-  $CXX $CXXFLAGS $LIB_FUZZING_ENGINE \
-    tests/fuzzer/${fuzzer}.o -o /${fuzzer} \
-    "${webp_libs[@]}"
-done
-
-for fuzzer in "${webp_cxx_fuzzers[@]}"; do
-  $CXX $CXXFLAGS -Isrc -I. $LIB_FUZZING_ENGINE \
-    tests/fuzzer/${fuzzer}.cc -o /${fuzzer} \
-    "${webp_libs[@]}"
-done
-
-for fuzzer in "${webp_c_fuzzers[@]}" "${webp_cxx_fuzzers[@]}"; do
-  cp tests/fuzzer/fuzz.dict /${fuzzer}.dict
-done
-
-#AFL++
-export CC=afl-clang-fast
-export CXX=afl-clang-fast++
-
-# limit allocation size to reduce spurious OOMs
-WEBP_CFLAGS="$CFLAGS -DWEBP_MAX_IMAGE_SIZE=838860800" # 800MiB
-
+# LAF
+cd ../libwebp-compcov
+git apply vhtc.patch
+./autogen.sh
 CFLAGS="$WEBP_CFLAGS" ./configure \
   --enable-asserts \
   --enable-libwebpdemux \
@@ -92,51 +86,12 @@ CFLAGS="$WEBP_CFLAGS" ./configure \
   --disable-gif \
   --disable-wic
 make clean
-make -j$(nproc)
+make CC=afl-clang-fast AFL_LLVM_LAF_ALL=1 -j$(nproc)
 
-webp_libs=(
-  src/demux/.libs/libwebpdemux.a
-  src/mux/.libs/libwebpmux.a
-  src/.libs/libwebp.a
-  imageio/.libs/libimageio_util.a
-  sharpyuv/.libs/libsharpyuv.a
-)
-webp_c_fuzzers=(
-  advanced_api_fuzzer
-  animation_api_fuzzer
-  huffman_fuzzer
-  mux_demux_api_fuzzer
-  simple_api_fuzzer
-)
-webp_cxx_fuzzers=(
-  animdecoder_fuzzer
-  animencoder_fuzzer
-  enc_dec_fuzzer
-)
-LIB_FUZZING_ENGINE="-fsanitize=fuzzer"
-for fuzzer in "${webp_c_fuzzers[@]}"; do
-  $CC $CFLAGS -Isrc -I. tests/fuzzer/${fuzzer}.c -c -o tests/fuzzer/${fuzzer}.o
-  $CXX $CXXFLAGS $LIB_FUZZING_ENGINE \
-    tests/fuzzer/${fuzzer}.o -o /afl_${fuzzer} \
-    "${webp_libs[@]}"
-done
-
-for fuzzer in "${webp_cxx_fuzzers[@]}"; do
-  $CXX $CXXFLAGS -Isrc -I. $LIB_FUZZING_ENGINE \
-    tests/fuzzer/${fuzzer}.cc -o /afl_${fuzzer} \
-    "${webp_libs[@]}"
-done
-
-#Sydr
-export CC=clang
-export CXX=clang++
-
-
-CFLAGS="-g"
-CXXFLAGS="-g"
-# limit allocation size to reduce spurious OOMs
-WEBP_CFLAGS="$CFLAGS -DWEBP_MAX_IMAGE_SIZE=838860800" # 800MiB
-
+# CTX
+cd ../libwebp-ctx
+git apply vhtc.patch
+./autogen.sh
 CFLAGS="$WEBP_CFLAGS" ./configure \
   --enable-asserts \
   --enable-libwebpdemux \
@@ -147,49 +102,12 @@ CFLAGS="$WEBP_CFLAGS" ./configure \
   --disable-gif \
   --disable-wic
 make clean
-make -j$(nproc)
+make CC=afl-clang-fast AFL_LLVM_INSTRUMENT=CTX -j$(nproc)
 
-webp_libs=(
-  src/demux/.libs/libwebpdemux.a
-  src/mux/.libs/libwebpmux.a
-  src/.libs/libwebp.a
-  imageio/.libs/libimageio_util.a
-  sharpyuv/.libs/libsharpyuv.a
-)
-webp_c_fuzzers=(
-  advanced_api_fuzzer
-  animation_api_fuzzer
-  huffman_fuzzer
-  mux_demux_api_fuzzer
-  simple_api_fuzzer
-)
-webp_cxx_fuzzers=(
-  animdecoder_fuzzer
-  animencoder_fuzzer
-  enc_dec_fuzzer
-)
-$CC $CFLAGS main.c -c -o main.o
-LIB_FUZZING_ENGINE="main.o"
-for fuzzer in "${webp_c_fuzzers[@]}"; do
-  $CC $CFLAGS -Isrc -I. tests/fuzzer/${fuzzer}.c -c -o tests/fuzzer/${fuzzer}.o
-  $CXX $CXXFLAGS $LIB_FUZZING_ENGINE -lpthread \
-    tests/fuzzer/${fuzzer}.o -o /sydr_${fuzzer} \
-    "${webp_libs[@]}"
-done
-
-for fuzzer in "${webp_cxx_fuzzers[@]}"; do
-  $CXX $CXXFLAGS -Isrc -I. $LIB_FUZZING_ENGINE -lpthread \
-    tests/fuzzer/${fuzzer}.cc -o /sydr_${fuzzer} \
-    "${webp_libs[@]}"
-done
-
-#Cov
-
-CFLAGS="-g -fprofile-instr-generate -fcoverage-mapping"
-CXXFLAGS="-g -fprofile-instr-generate -fcoverage-mapping"
-# limit allocation size to reduce spurious OOMs
-WEBP_CFLAGS="$CFLAGS -DWEBP_MAX_IMAGE_SIZE=838860800" # 800MiB
-
+# NGRAM-8
+cd ../libwebp-ngram
+git apply vhtc.patch
+./autogen.sh
 CFLAGS="$WEBP_CFLAGS" ./configure \
   --enable-asserts \
   --enable-libwebpdemux \
@@ -200,38 +118,4 @@ CFLAGS="$WEBP_CFLAGS" ./configure \
   --disable-gif \
   --disable-wic
 make clean
-make -j$(nproc)
-
-webp_libs=(
-  src/demux/.libs/libwebpdemux.a
-  src/mux/.libs/libwebpmux.a
-  src/.libs/libwebp.a
-  imageio/.libs/libimageio_util.a
-  sharpyuv/.libs/libsharpyuv.a
-)
-webp_c_fuzzers=(
-  advanced_api_fuzzer
-  animation_api_fuzzer
-  huffman_fuzzer
-  mux_demux_api_fuzzer
-  simple_api_fuzzer
-)
-webp_cxx_fuzzers=(
-  animdecoder_fuzzer
-  animencoder_fuzzer
-  enc_dec_fuzzer
-)
-$CC $CFLAGS main.c -c -o main.o
-LIB_FUZZING_ENGINE="main.o"
-for fuzzer in "${webp_c_fuzzers[@]}"; do
-  $CC $CFLAGS -Isrc -I. tests/fuzzer/${fuzzer}.c -c -o tests/fuzzer/${fuzzer}.o
-  $CXX $CXXFLAGS $LIB_FUZZING_ENGINE -lpthread \
-    tests/fuzzer/${fuzzer}.o -o /cov_${fuzzer} \
-    "${webp_libs[@]}"
-done
-
-for fuzzer in "${webp_cxx_fuzzers[@]}"; do
-  $CXX $CXXFLAGS -Isrc -I. $LIB_FUZZING_ENGINE -lpthread \
-    tests/fuzzer/${fuzzer}.cc -o /cov_${fuzzer} \
-    "${webp_libs[@]}"
-done
+make CC=afl-clang-fast AFL_LLVM_INSTRUMENT=NGRAM-8 -j$(nproc)
