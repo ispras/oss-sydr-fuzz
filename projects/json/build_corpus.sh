4f967e83083c4c48dab82babb867d3a28040cf3c
Avgor46 <avgor46@ispras.ru>
2024-03-21 13:25:18 +0300

    Change number of jobs + update build_corpus.sh

--- projects/json/build_corpus.sh
+++ projects/json/build_corpus.sh
@@ -9,3 +9,4 @@ do
   find json_test_data-$TEST_DATA_VERSION -size -5k -name "*.$FORMAT" -exec cp "{}" "corpus_$FORMAT" \;
 done
 rm -fr json_test_data-$TEST_DATA_VERSION
+rm corpus_json/n_structure_no_data.json # removing empty file from corpus
