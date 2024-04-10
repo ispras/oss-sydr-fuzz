5b6945b1f0179d752dfd4b76c4e1f7f4845d69a5
headshog <craaaaaachind@gmail.com>
2024-04-10 16:36:56 +0300

    Format target

--- projects/cppsharp/Parser.cs
+++ projects/cppsharp/Parser.cs
@@ -32,7 +32,6 @@ namespace CppSharp
                     Console.Error.WriteLine("A path to a file for parsing is required.");
                     return;
                 }
-
                 var file = Path.GetFullPath(args[0]);
                 ParseSourceFile(file);
             });
@@ -40,24 +39,13 @@ namespace CppSharp
 
         private static bool ParseSourceFile(string file)
         {
-            // Lets setup the options for parsing the file.
             var parserOptions = new ParserOptions
             {
                 LanguageVersion = LanguageVersion.CPP20_GNU,
-
-                // Verbose here will make sure the parser outputs some extra debugging
-                // information regarding include directories, which can be helpful when
-                // tracking down parsing issues.
                 Verbose = true
             };
-
-            // This will setup the necessary system include paths and arguments for parsing.
             parserOptions.Setup(Platform.Host);
-
-            // We create the Clang parser and parse the source code.
             var parserResult = ClangParser.ParseSourceFile(file, parserOptions);
-
-            // If there was some kind of error parsing, then lets print some diagnostics.
             if (parserResult.Kind != ParserResultKind.Success)
             {
                 if (parserResult.Kind == ParserResultKind.FileNotFound)
@@ -71,24 +59,13 @@ namespace CppSharp
                         diag.FileName, diag.LineNumber, diag.ColumnNumber,
                         diag.Level.ToString().ToLower(), diag.Message);
                 }
-
                 parserResult.Dispose();
                 return false;
             }
-
-            // Now we can consume the output of the parser (syntax tree).
-
-            // First we will convert the output, bindings for the native Clang AST,
-            // to CppSharp's managed AST representation.
             var astContext = ClangParser.ConvertASTContext(parserOptions.ASTContext);
-
-            // After its converted, we can dispose of the native AST bindings.
             parserResult.Dispose();
-
-            // Now we can finally do what we please with the syntax tree.
             foreach (var sourceUnit in astContext.TranslationUnits)
                 Console.WriteLine(sourceUnit.FileName);
-
             return true;
         }
     }
