46cdd523bc865405da10baf846187a6d3d417e00
Darya Parygina <pa_darochek@ispras.ru>
2024-09-09 13:23:37 +0300

    Fixes

--- projects/nginx/auth_fuzz/build.sh
+++ projects/nginx/auth_fuzz/build.sh
@@ -1,4 +1,32 @@
 #!/bin/bash
+# Copyright 2024 ISP RAS
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+
+# Make and install ansifilter
+ANSIFILTER_SRC_DIR="/ansifilter"
+cd ${ANSIFILTER_SRC_DIR}
+env CC=clang CXX=clang++ make
+make install
+cd /
+rm -rf ${ANSIFILTER_SRC_DIR}
+
+# Make preeny
+cd preeny
+make
+cd /
 
 NGINX_DIR=$1
 PATCHES_DIR=$2
