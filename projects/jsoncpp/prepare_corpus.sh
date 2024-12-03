36029ff7446413758f166524119249f469bcc95c
Daniel Kuts <kutz@ispras.ru>
2024-12-03 18:14:05 +0300

    [jsoncpp] Update prepare_corpus.sh

--- projects/jsoncpp/prepare_corpus.sh
+++ projects/jsoncpp/prepare_corpus.sh
@@ -7,5 +7,5 @@ for filename in /corpus/*.json; do
     echo "";
     echo "Try to process: ${filename}";
     name=${filename##*}
-    /pack/json_packer "${filename}" "/proto${filename}.pb" "-l" || true;
+    /pack/json_packer "${filename}" "/proto${filename}.pb" "--to-proto" || true;
 done
