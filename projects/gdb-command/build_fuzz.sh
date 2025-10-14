63270fe29e9d2760b259b39d943786f9bcae9a30
headshog <craaaaaachind@gmail.com>
2025-10-14 18:49:20 +0300

    Upd copyrights

--- projects/gdb-command/build_fuzz.sh
+++ projects/gdb-command/build_fuzz.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -eu
 # Copyright 2021 Google LLC
+# Modifications copyright (C) 2025 ISP RAS
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
