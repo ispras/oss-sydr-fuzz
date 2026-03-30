48831fc577bdfca1099c8cb230cb3bc2f1ea9156
Danil <danya541@bk.ru>
2026-03-30 11:03:26 +0300

    Behaviortreecpp (#324)

---
+++ projects/behaviortreecpp/README.md
@@ -0,0 +1,62 @@
+# behaviortreecpp
+
+behaviortreecpp is a C++ library for creating, executing, and testing behavior trees.
+
+## Build Docker
+
+    $ sudo docker build -t oss-sydr-fuzz-behaviortreecpp .
+
+## Run Hybrid Fuzzing
+
+Unzip Sydr (`sydr.zip`) in `projects/behaviortreecpp` directory:
+
+    $ unzip sydr.zip
+
+Run docker:
+
+    $ sudo docker run --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -v /etc/localtime:/etc/localtime:ro --rm -it -v $PWD:/fuzz oss-sydr-fuzz-behaviortreecpp /bin/bash
+
+Change directory to `/fuzz`:
+
+    # cd /fuzz
+
+Run hybrid fuzzing with libfuzzer:
+
+    # sydr-fuzz -c tomls/bt-lf.toml run
+
+Run hybrid fuzzing with afl++:
+
+    # sydr-fuzz -c tomls/bt-afl++.toml run
+
+Minimize corpus:
+
+    # sydr-fuzz -c tomls/bt-lf.toml cmin
+
+Check security predicates:
+
+    # sydr-fuzz -c tomls/bt-lf.toml security
+
+Crash triage with CASR:
+
+    # sydr-fuzz -c tomls/bt-lf.toml casr
+
+Get LCOV HTML coverage report:
+
+    # sydr-fuzz -c tomls/bt-lf.toml cov-export -- -format=lcov > bt.lcov
+    # genhtml -o bt-html bt.lcov
+
+## Alternative Fuzz Targets
+
+behaviortreecpp project has 3 fuzz targets.
+
+### libfuzzer
+
+    # sydr-fuzz -c tomls/bb-lf.toml     run
+    # sydr-fuzz -c tomls/bt-lf.toml     run
+    # sydr-fuzz -c tomls/script-lf.toml run
+    
+### afl++
+
+    # sydr-fuzz -c tomls/bb-afl++.toml     run
+    # sydr-fuzz -c tomls/bt-afl++.toml     run
+    # sydr-fuzz -c tomls/script-afl++.toml run
---
+++ projects/behaviortreecpp/build.sh
@@ -0,0 +1,111 @@
+#!/bin/bash -eu
+# Copyright 2025 Google LLC
+# Modifications copyright (C) 2026 ISP RAS
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+
+if [[ $TARGET == "fuzzer" ]]
+then
+    export CC=clang
+    export CXX=clang++
+    export CFLAGS="-g -fsanitize=fuzzer-no-link,address,bounds,null,float-divide-by-zero"
+    export CXXFLAGS=$CFLAGS
+elif [[ $TARGET == "afl" ]]
+then
+    export CC=afl-clang-fast
+    export CXX=afl-clang-fast++
+    export CFLAGS="-g -fsanitize=address,bounds,null,float-divide-by-zero"
+    export CXXFLAGS=$CFLAGS
+elif [[ $TARGET == "sydr" ]]
+then
+    export CC=clang
+    export CXX=clang++
+    export CFLAGS="-g"
+    export CXXFLAGS=$CFLAGS
+elif [[ $TARGET == "cov" ]]
+then
+    export CC=clang
+    export CXX=clang++
+    export CFLAGS="-g -fprofile-instr-generate -fcoverage-mapping"
+    export CXXFLAGS=$CFLAGS
+fi
+
+# ===== Build Sqlite =====
+SQLITE_VER=sqlite-autoconf-3480000
+wget https://www.sqlite.org/2025/${SQLITE_VER}.tar.gz
+tar xzf ${SQLITE_VER}.tar.gz
+cd ${SQLITE_VER}
+./configure --enable-static --disable-shared
+make -j"$(nproc)"
+make install
+cd ..
+
+# ===== Build zeroMQ =====
+git clone https://github.com/zeromq/libzmq.git
+cd libzmq
+mkdir build && cd build
+cmake .. -DBUILD_SHARED=OFF -DBUILD_STATIC=ON -DZMQ_BUILD_TESTS=OFF
+make -j"$(nproc)"
+make install
+cd ../..
+
+# ===== Build BehaviorTree.CPP =====
+mkdir "/${TARGET}"
+mkdir build && cd build
+
+if [[ $TARGET == "sydr" || $TARGET == "cov" ]]
+then
+    $CC $CFLAGS -c /opt/StandaloneFuzzTargetMain.c -o main_$TARGET.o
+    export LIB_FUZZING_ENGINE="${PWD}/main_$TARGET.o"
+fi
+
+CMAKE_FLAGS=(
+  "-DCMAKE_BUILD_TYPE=Release"
+  "-DENABLE_FUZZING=ON"
+  "-DFORCE_STATIC_LINKING=ON"
+  "-DBUILD_TESTING=OFF"
+)
+
+cmake .. "${CMAKE_FLAGS[@]}"
+make -j"$(nproc)"
+
+for fuzz_target in bb bt script; do
+    cp "${fuzz_target}_fuzzer" "/${TARGET}/${fuzz_target}_${TARGET}"
+done
+
+if [[ $TARGET == "fuzzer" ]]
+then
+    for fuzz_target in bb bt script; do
+      if [ -d "../fuzzing/corpus/${fuzz_target}_corpus" ]; then
+        cp -r "../fuzzing/corpus/${fuzz_target}_corpus" "/corpus_${fuzz_target}"
+      fi
+    done
+fi
+
+# ===== Clean dependencies =====
+cd /behaviortreecpp/${SQLITE_VER}
+make clean
+make uninstall
+
+cd /behaviortreecpp/libzmq/build
+make clean
+
+# Use this because there is no make uninstall
+xargs -d '\n' rm -f < install_manifest.txt 
+
+rm -rf /behaviortreecpp/build
+rm -rf /behaviortreecpp/${SQLITE_VER}
+rm -f  /behaviortreecpp/${SQLITE_VER}.tar.gz
+rm -rf /behaviortreecpp/libzmq
