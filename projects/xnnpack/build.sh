a202dc9f0e5faaf7f1fcde484265f9001650e9ba
Atlaxsyys <y.shafran-06@mail.ru>
2026-04-01 22:22:27 +0300

    [xnnpack]: adapt build.sh && add fuzz_model.cc && fixe Dockerfile

--- projects/xnnpack/build.sh
+++ projects/xnnpack/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -eu
 # Copyright 2023 Google LLC
+# Modifications copyright (C) 2026 ISP RAS
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,22 +16,45 @@
 #
 ################################################################################
 
-mkdir build
-cd build
-cmake -DXNNPACK_BUILD_BENCHMARKS=OFF -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DXNNPACK_BUILD_TESTS=ON ../
+export CC=clang
+export CXX=clang++
+export CFLAGS="-g -fsanitize=fuzzer-no-link,address,undefined"
+export CXXFLAGS="-g -fsanitize=fuzzer-no-link,address,undefined"
+
+mkdir -p build_fuzz
+cd build_fuzz
+
+cmake \
+    -DXNNPACK_BUILD_BENCHMARKS=OFF \
+    -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
+    -DXNNPACK_BUILD_TESTS=ON \
+    -DCMAKE_C_COMPILER="$CC" \
+    -DCMAKE_CXX_COMPILER="$CXX" \
+    -DCMAKE_C_FLAGS="$CFLAGS" \
+    -DCMAKE_CXX_FLAGS="$CXXFLAGS" \
+    ../
+
 make V=1 -j8
 cd ../
 
-$CXX $CXXFLAGS $LIB_FUZZING_ENGINE ../fuzz_model.cc \
+export CFLAGS="-g -fsanitize=fuzzer,address,undefined"
+export CXXFLAGS="-g -fsanitize=fuzzer,address,undefined"
+
+$CXX $CXXFLAGS ./fuzz_model.cc \
     -DFXDIV_USE_INLINE_ASSEMBLY=0 -DPTHREADPOOL_NO_DEPRECATED_API=1 \
     -DXNN_ENABLE_ARM_BF16=1 -DXNN_ENABLE_ARM_DOTPROD=1 \
     -DXNN_ENABLE_ARM_FP16_SCALAR=1 -DXNN_ENABLE_ARM_FP16_VECTOR=1 \
     -DXNN_ENABLE_ASSEMBLY=1 -DXNN_ENABLE_DWCONV_MULTIPASS=0 \
     -DXNN_ENABLE_GEMM_M_SPECIALIZATION=1 -DXNN_ENABLE_JIT=0 \
     -DXNN_ENABLE_MEMOPT=1 -DXNN_ENABLE_RISCV_VECTOR=1 -DXNN_ENABLE_SPARSE=1 \
-    -I/src/xnnpack/src -I/src/xnnpack/build/pthreadpool-source/include \
-    -I/src/xnnpack/build/FXdiv-source/include -I/src/xnnpack/include/ \
-    -I/src/xnnpack/build/FP16-source/include ./build/libXNNPACK.a \
-    ./build/pthreadpool/libpthreadpool.a ./build/cpuinfo/libcpuinfo.a \
-    ./build/libxnnpack-microkernels-all.a ./build/libxnnpack-microkernels-prod.a \
-    -o $OUT/fuzz_model
+    -I/xnnpack/src \
+    -I/xnnpack/build_fuzz/pthreadpool-source/include \
+    -I/xnnpack/build_fuzz/FXdiv-source/include \
+    -I/xnnpack/include \
+    -I/xnnpack/build_fuzz/FP16-source/include \
+    ./build_fuzz/libXNNPACK.a \
+    ./build_fuzz/pthreadpool/libpthreadpool.a \
+    ./build_fuzz/cpuinfo/libcpuinfo.a \
+    ./build_fuzz/libxnnpack-microkernels-all.a \
+    ./build_fuzz/libxnnpack-microkernels-prod.a \
+    -o fuzz_model_fuzz
