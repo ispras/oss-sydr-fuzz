f0f0c11165363de6a51730a5a2095651f29b6b71
Samir Nezametdinov <nezametdinovsamir@gmail.com>
2026-04-15 16:28:12 +0300

    full work version

--- projects/vulkan-loader/build.sh
+++ projects/vulkan-loader/build.sh
@@ -14,7 +14,61 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-################################################################################
+################################################################################   Fuzzing Build
+
+export CC=clang
+export CXX=clang++
+export CFLAGS="-g -fsanitize=fuzzer-no-link,address,undefined"
+export CXXFLAGS="-g -fsanitize=fuzzer-no-link,address,undefined"
+
+cd /vulkan-loader
+git apply --ignore-space-change --ignore-whitespace /fuzz-patch.diff
+
+mkdir -p build
+cd build
+
+sed -i 's/fput/\/\/fput/g' /vulkan-loader/loader/log.c
+
+cmake -DUPDATE_DEPS=ON -DCMAKE_BUILD_TYPE=Release ..
+make -j$(nproc)
+
+ar rcs /libvulkan.a /vulkan-loader/build/loader/CMakeFiles/vulkan.dir/*.o
+
+$CC $CFLAGS -I/vulkan-loader/loader \
+    -I/vulkan-loader/loader/generated \
+    -I/vulkan-headers/include \
+    -I/ \
+    -DENABLE_FILE_CALLBACK \
+    -c /instance_create_advanced_fuzzer.c -o instance_create_advanced_fuzzer.o
+
+$CXX -g -fsanitize=fuzzer,address,undefined instance_create_advanced_fuzzer.o \
+    -o /instance_create_advanced_fuzzer -lpthread /libvulkan.a
+cp /vulkan-keywords.dict /instance_create_advanced_fuzzer.dict
+
+$CC $CXXFLAGS -I/vulkan-loader/loader \
+    -I/vulkan-loader/loader/generated \
+    -I/vulkan-headers/include \
+    -DSPLIT_INPUT \
+    -c /instance_enumerate_fuzzer.c -o instance_enumerate_fuzzer_split_input.o
+
+$CXX -g -fsanitize=fuzzer,address,undefined instance_enumerate_fuzzer_split_input.o \
+    -o /instance_enumerate_fuzzer_split_input -lpthread /libvulkan.a
+cp /vulkan-keywords.dict /instance_enumerate_fuzzer_split_input.dict
+
+for fuzzer in instance_create_fuzzer json_load_fuzzer settings_fuzzer instance_enumerate_fuzzer; do
+    #fuzz_basename=$(basename -s .c $fuzzers)
+    $CC $CXXFLAGS -I/vulkan-loader/loader \
+        -I/vulkan-loader/loader/generated \
+        -I/vulkan-headers/include \
+        -c /$fuzzer.c -o $fuzzer.o
+
+    $CXX -g -fsanitize=fuzzer,address,undefined $fuzzer.o \
+        -o /$fuzzer -lpthread /libvulkan.a
+
+    cp /vulkan-keywords.dict /$fuzzer.dict
+done
+
+################################################################################# Coverage Build
 
 export CC=clang
 export CXX=clang++
@@ -24,7 +78,6 @@ export CXXFLAGS="-g -fprofile-instr-generate -fcoverage-mapping"
 rm -rf /vulkan-loader/build
 mkdir -p /vulkan-loader/build
 cd /vulkan-loader
-git apply --ignore-space-change --ignore-whitespace /fuzz-patch.diff
 
 mkdir -p build
 cd build
