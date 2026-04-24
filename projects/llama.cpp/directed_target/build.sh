6316fb87f1a9221fcefb00172d21da418969b939
headshog <craaaaaachind@gmail.com>
2026-04-24 14:49:13 +0300

    Update libafl makefiles with cache

--- projects/llama.cpp/directed_target/build.sh
+++ projects/llama.cpp/directed_target/build.sh
@@ -47,32 +47,36 @@ sed -i 's/ok = ok \&\& (info->n_dims <= GGML_MAX_DIMS);/ok = ok \&\& (info->n_di
 rm -rf $OUT
 mkdir -p $OUT
 
+build_lib() {
+    cmake -B build$LIBAFL_SHARED_NAME -DLLAMA_CURL=OFF  -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX -DBUILD_SHARED_LIBS=OFF -DLLAMA_BUILD_SERVER=OFF
+    cmake --build build$LIBAFL_SHARED_NAME --config Debug -j16
+    $CC $CFLAGS /opt/StandaloneFuzzTargetMain.c -c -o /StandaloneFuzzTargetMain.o
+
+    # Convert models into header files so we can use them for fuzzing.
+    xxd -i models/ggml-vocab-bert-bge.gguf > model_header_bge.h
+    xxd -i models/ggml-vocab-llama-bpe.gguf > model_header_bpe.h
+    xxd -i models/ggml-vocab-llama-spm.gguf > model_header_spm.h
+    xxd -i models/ggml-vocab-qwen2.gguf > model_header_qwen2.h
+    xxd -i models/ggml-vocab-command-r.gguf > model_header_command_r.h
+    xxd -i models/ggml-vocab-aquila.gguf > model_header_aquila.h
+    xxd -i models/ggml-vocab-gpt-2.gguf > model_header_gpt_2.h
+    xxd -i models/ggml-vocab-baichuan.gguf > model_header_baichuan.h
+    xxd -i models/ggml-vocab-deepseek-coder.gguf > model_header_deepseek_coder.h
+    xxd -i models/ggml-vocab-falcon.gguf > model_header_falcon.h
+
+    mkdir -p ./build$LIBAFL_SHARED_NAME/myos
+    find "./build$LIBAFL_SHARED_NAME/ggml/"   -name '*.o' -exec cp {} "./build$LIBAFL_SHARED_NAME/myos/" \;
+    find "./build$LIBAFL_SHARED_NAME/src/"    -name '*.o' -exec cp {} "./build$LIBAFL_SHARED_NAME/myos/" \;
+    find "./build$LIBAFL_SHARED_NAME/common/" -name '*.o' -exec cp {} "./build$LIBAFL_SHARED_NAME/myos/" \;
+}
+
 # For non-libafl target we can build libs only once.
 if [[ "$TARGET" != "libafl" ]]
 then
-    cmake -B build -DLLAMA_CURL=OFF  -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX -DBUILD_SHARED_LIBS=OFF -DLLAMA_BUILD_SERVER=OFF
-    cmake --build build --config Debug -j16
-    $CC $CFLAGS /opt/StandaloneFuzzTargetMain.c -c -o /StandaloneFuzzTargetMain.o
+    export LIBAFL_SHARED_NAME=""
+    build_lib
 fi
 
-# Convert models into header files so we can use them for fuzzing.
-xxd -i models/ggml-vocab-bert-bge.gguf > model_header_bge.h
-xxd -i models/ggml-vocab-llama-bpe.gguf > model_header_bpe.h
-xxd -i models/ggml-vocab-llama-spm.gguf > model_header_spm.h
-xxd -i models/ggml-vocab-qwen2.gguf > model_header_qwen2.h
-xxd -i models/ggml-vocab-command-r.gguf > model_header_command_r.h
-xxd -i models/ggml-vocab-aquila.gguf > model_header_aquila.h
-xxd -i models/ggml-vocab-gpt-2.gguf > model_header_gpt_2.h
-xxd -i models/ggml-vocab-baichuan.gguf > model_header_baichuan.h
-xxd -i models/ggml-vocab-deepseek-coder.gguf > model_header_deepseek_coder.h
-xxd -i models/ggml-vocab-falcon.gguf > model_header_falcon.h
-
-mkdir ./build/myos
-find ./build/ggml/ -name *.o -exec cp {} ./build/myos/ \;
-find ./build/src/ -name *.o -exec cp {} ./build/myos/ \;
-find ./build/common/ -name *.o -exec cp {} ./build/myos/ \;
-
-OBJ_FILES="/llama.cpp/build/myos/*.o"
 FLAGS="-std=c++17 -Iggml/include -Iggml/src -Iinclude -Isrc -Icommon -Ivendor -I./ -DNDEBUG -DGGML_USE_LLAMAFILE"
 LIB_FUZZING_ENGINE=$ENGINE
 
@@ -84,19 +88,17 @@ build_target(){
     if [[ "$TARGET" == "libafl" ]]
     then
         export LIBAFL_SHARED_NAME="llama_${target}"
-        cmake -B build -DLLAMA_CURL=OFF -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX -DBUILD_SHARED_LIBS=OFF -DLLAMA_BUILD_SERVER=OFF
-        cmake --build build --config Debug -j16
-        $CC $CFLAGS /opt/StandaloneFuzzTargetMain.c -c -o /StandaloneFuzzTargetMain.o
+        build_lib
     fi
 
     # Build target
     $CXX $CXXFLAGS ${FLAGS} fuzzers/$target.cpp -c -o ./$target.o
     $CXX $CXXFLAGS ${FLAGS} ./$target.o \
-        -Wl,--whole-archive,"/llama.cpp/build/src/libllama.a" -Wl,--no-whole-archive \
-        -WL,--whole-archive,"/llama.cpp/build/ggml/src/libggml-cpu.a" -Wl,--no-whole-archive \
-        -ldl -pthread -lm -lrt /llama.cpp/build/ggml/src/libggml-cpu.a ./build/ggml/src/libggml-base.a \
-        ./build/ggml/src/libggml.a ./build/common/libcommon.a \
-        /llama.cpp/build/ggml/src/libggml-cpu.a $LIB_FUZZING_ENGINE \
+        -Wl,--whole-archive,"/llama.cpp/build$LIBAFL_SHARED_NAME/src/libllama.a" -Wl,--no-whole-archive \
+        -WL,--whole-archive,"/llama.cpp/build$LIBAFL_SHARED_NAME/ggml/src/libggml-cpu.a" -Wl,--no-whole-archive \
+        -ldl -pthread -lm -lrt /llama.cpp/build$LIBAFL_SHARED_NAME/ggml/src/libggml-cpu.a ./build$LIBAFL_SHARED_NAME/ggml/src/libggml-base.a \
+        ./build$LIBAFL_SHARED_NAME/ggml/src/libggml.a ./build$LIBAFL_SHARED_NAME/common/libcommon.a \
+        /llama.cpp/build$LIBAFL_SHARED_NAME/ggml/src/libggml-cpu.a $LIB_FUZZING_ENGINE \
         -o $OUT/${target}_$TARGET
 }
 
@@ -115,26 +117,29 @@ build_tokenizer(){
 
     if [[ "$TARGET" == "libafl" ]]
     then
-        export LIBAFL_SHARED_NAME="llama_${target}"
-        cmake -B build -DLLAMA_CURL=OFF -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX -DBUILD_SHARED_LIBS=OFF -DLLAMA_BUILD_SERVER=OFF
-        cmake --build build --config Debug -j16
-        $CC $CFLAGS /opt/StandaloneFuzzTargetMain.c -c -o /StandaloneFuzzTargetMain.o
+        export LIBAFL_SHARED_NAME="llama_fuzz_tokenizer_${target}"
+        build_lib
     fi
 
     # Build target
     $CXX $CXXFLAGS ${FLAGS} fuzzers/fuzz_tokenizer.cpp -c -o ./fuzz_tokenizer_${target}.o
     $CXX $CXXFLAGS ${FLAGS} -DFUZZ_$flag ./fuzz_tokenizer_$target.o \
-        -Wl,--whole-archive,"/llama.cpp/build/src/libllama.a" -Wl,--no-whole-archive \
-        -WL,--whole-archive,"/llama.cpp/build/ggml/src/libggml-cpu.a" -Wl,--no-whole-archive \
-        -ldl -pthread -lm -lrt /llama.cpp/build/ggml/src/libggml-cpu.a ./build/ggml/src/libggml-base.a \
-        ./build/common/libcommon.a ./build/ggml/src/libggml.a ./build/common/libcommon.a \
-        /llama.cpp/build/ggml/src/libggml-cpu.a $LIB_FUZZING_ENGINE \
+        -Wl,--whole-archive,"/llama.cpp/build$LIBAFL_SHARED_NAME/src/libllama.a" -Wl,--no-whole-archive \
+        -WL,--whole-archive,"/llama.cpp/build$LIBAFL_SHARED_NAME/ggml/src/libggml-cpu.a" -Wl,--no-whole-archive \
+        -ldl -pthread -lm -lrt /llama.cpp/build$LIBAFL_SHARED_NAME/ggml/src/libggml-cpu.a ./build$LIBAFL_SHARED_NAME/ggml/src/libggml-base.a \
+        ./build$LIBAFL_SHARED_NAME/common/libcommon.a ./build$LIBAFL_SHARED_NAME/ggml/src/libggml.a ./build$LIBAFL_SHARED_NAME/common/libcommon.a \
+        /llama.cpp/build$LIBAFL_SHARED_NAME/ggml/src/libggml-cpu.a $LIB_FUZZING_ENGINE \
         -o $OUT/fuzz_tokenizer_${target}_$TARGET
 }
 
 token_flags=("BGE" "BPE" "SPM" "COMMAND_R" "AQUILA" "QWEN2" "GPT_2" "BAICHUAN" "DEEPSEEK_CODER" "FALCON")
 for fuzztarget in ${token_flags[@]}; do
-    build_tokenizer $fuzztarget &
+    if [[ "$TARGET" == "libafl" ]]
+    then
+        build_tokenizer $fuzztarget
+    else
+        build_tokenizer $fuzztarget &
+    fi
 done
 
 wait
